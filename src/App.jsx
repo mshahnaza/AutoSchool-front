ff9377308b910ab1cddc4c31d7b294aceb2dfbca
mshahnaza <mamirovasahnaza042@gmail.com>
2025-05-23 00:36:53 +0600

    car animation added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Tabs from "./components/Schedule/Tabs";
+import RoadAnimation from "./components/RoadAnimation";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/schedule" element={<Tabs />} />
         {/* Add other routes here */}
       </Routes>
+      <RoadAnimation />
       <Footer />
     </Router>
   );
---
+++ src/components/RoadAnimation.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import carIcon from "../assets/icons/image.svg";
+import "./styles/RoadAnimation.css";
+
+export default function RoadAnimation() {
+  return (
+    <div className="road-animation-bg">
+      <svg className="road-svg" viewBox="0 0 2500 800" xmlns="http://www.w3.org/2000/svg">
+        <path
+          id="roadPath"
+          d="M 0,400 
+             C 300,350 300,100 600,150 
+             S 900,450 1200,350 
+             S 1500,0 1800,200 
+             S 2200,400 2500,350"
+          stroke="#a0a0a0"
+          strokeWidth="10"
+          fill="none"
+          strokeLinecap="round"
+          strokeDasharray="20, 10"
+        />
+      </svg>
+
+      <img src={carIcon} alt="Car" className="car-on-road" />
+    </div>
+  );
+}
